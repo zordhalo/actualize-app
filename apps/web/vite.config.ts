85548d507a2fb01847051fe8c4fed0f28052a88d
Lukce <krawczakluke@gmail.com>
2026-01-10 16:46:57 -0500

    Ignore config errors in tsconfigPaths for Vite
    Updates the tsconfigPaths plugin in the Vite config to use the 'ignoreConfigErrors: true' option, allowing the build to proceed even if there are TypeScript config errors.

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -63,7 +63,7 @@ export default defineConfig({
     loadFontsFromTailwindSource(),
     addRenderIds(),
     reactRouter(),
-    tsconfigPaths(),
+    tsconfigPaths({ ignoreConfigErrors: true }),
     aliases(),
     layoutWrapperPlugin(),
   ],
